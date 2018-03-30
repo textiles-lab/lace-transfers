4dc366db5e72c79bbff18da4e8432b4c6d4ed64b
Jenny <jhl989@mit.edu>
2018-03-30 16:05:11 -0400

    well, it's not explicitly failing any test cases anymore

--- school-bus-bridge.js
+++ school-bus-bridge.js
@@ -1,58 +1,43 @@
 function multi_pass_transfers(offsets, firsts, minRacking, maxRacking, xfer) {
 
-/*	var tempGoal = fill_all_valleys(offsets, firsts);
-	print_goal(tempGoal, firsts);
-
-*/	
 	var currentOffset = 0;
 	let needle_pos = [];
+	let on_front = [];
 	for (let i = 0; i < offsets.length; ++i) {
 		needle_pos[i] = i;
+		on_front[i] = true;
 	}
 
 
 	for (let i = 0; i < offsets.length; ++i) {
 		xfer('f', i, 'b', i-currentOffset);
+		on_front[i] = false;
 	}
-/*	currentOffset = minRacking; //TODO: check whether starting with maxRacking would be better 
-	while (currentOffset < maxRacking) {
-		++currentOffset
-		for (let i = 0; i < offsets.length; ++i) {
-			if (tempGoal[i]==currentOffset) {
-				xfer('b', i, 'f', i+currentOffset);
-				needle_pos[i] = i+currentOffset;
-			}
-		}
-	}*/
 
 	function min_to_max_pass(goal, startOffset) {
-		//let tempGoal = fill_all_valleys(goal, firsts);
 		print_goal(goal, firsts);
 		while (currentOffset < maxRacking) {
 			++currentOffset;
+			console.log(currentOffset - startOffset);
 			for (let i = 0; i < goal.length; ++i) {
-				if (goal[i] == currentOffset-startOffset) {
+				if (goal[i] == currentOffset-startOffset && !on_front[i]) {
 					xfer('b', needle_pos[i], 'f', needle_pos[i]+currentOffset);
+					on_front[i] = true;
 					needle_pos[i] = needle_pos[i]+currentOffset;
 				}
 			}
 		}
-
-		//return Array.from(goal, (x, i) => x - tempGoal[i]);
 	}
 
 	function max_to_min_pass(goal, startOffset) {
-		//let tempGoal = flatten_all_hills(goal, firsts);
-		//console.log("flattened hill goal is: ");
 		print_goal(goal, firsts);
 		while (currentOffset >= minRacking) {
-			console.log(currentOffset - startOffset);
 			currentOffset--;
+			console.log(currentOffset - startOffset);
 			for (let i = 0; i < goal.length; ++i) {
-				//started at max racking, so adjust offsets for that
-				if (goal[i] == currentOffset - startOffset) {
-					//transfer
+				if (goal[i] == currentOffset - startOffset  && !on_front[i]) {
 					xfer('b', needle_pos[i], 'f', needle_pos[i]+currentOffset);
+					on_front[i] = true;
 					needle_pos[i] = needle_pos[i] + currentOffset;
 				}
 			}
@@ -62,52 +47,37 @@ function multi_pass_transfers(offsets, firsts, minRacking, maxRacking, xfer) {
 
 	var increasing = true;
 	currentOffset = increasing ? (minRacking-1) : (maxRacking+1); //for the very first pass, everything has to go to the back bed
-	var tempGoal = increasing ? fill_all_valleys(offsets, firsts) : flatten_all_hills(offsets, firsts);
+	var tempGoal = increasing ? make_right_leaning_decreases(offsets, firsts) : make_left_leaning_decreases(offsets, firsts);
+	console.log("result of processing");
+	print_goal(tempGoal, firsts);
 	increasing ? min_to_max_pass(tempGoal, 0, minRacking) : max_to_min_pass(tempGoal, 0, maxRacking);
 	var middleGoal = Array.from(offsets, (x,i) => x - tempGoal[i]);
+	console.log("result after running processing");
 	print_goal(middleGoal, firsts);
 	while (!is_done(middleGoal)) {
 		increasing = !increasing;
-		tempGoal = increasing ? fill_all_valleys(middleGoal, firsts) : flatten_all_hills(middleGoal, firsts);
+		tempGoal = increasing ? make_right_leaning_decreases(middleGoal, firsts) : make_left_leaning_decreases(middleGoal, firsts);
+		console.log("result of processing");
+		print_goal(tempGoal, firsts);
+		
+		currentOffset = 0;
 		for (let i=0; i < offsets.length; ++i) {
-			if (tempGoal[i] != 0) {
+			//if (tempGoal[i] != 0) {
 				xfer('f', needle_pos[i], 'b', needle_pos[i]-currentOffset);
+				on_front[i] = false;
 				needle_pos[i] = needle_pos[i] - currentOffset;
-			}
+			//}
 		}
-		//increasing ? currentOffset++ : currentOffset--;
-		increasing ? min_to_max_pass(tempGoal, minRacking) : max_to_min_pass(tempGoal, maxRacking);
+		currentOffset = increasing ? (minRacking-1) : (maxRacking+1);
+		increasing ? min_to_max_pass(tempGoal, 0) : max_to_min_pass(tempGoal, 0);//assume start from zero, like we always start
 		middleGoal = Array.from(middleGoal, (x,i) => x - tempGoal[i]);
+		console.log("result after running processing");
 		print_goal(middleGoal, firsts);
 	}
 
 
 
 
-}
-
-function fill_all_valleys(offsets, firsts) {
-	var filledOffsets = [];
-	var index = 0;
-
-	while (index < offsets.length) {
-		filledOffsets[index] = offsets[index];
-		if (firsts[index]) {
-			//found a front stitch, do fill checks
-			let floor = offsets[index];
-			++index;
-			while (index < offsets.length && offsets[index] < floor) {
-				filledOffsets[index] = floor;
-				++index;
-			}
-		}
-		else {
-			//no need to fill, just continue
-			++index;
-		}
-	}
-
-	return filledOffsets;
 }
 
 function make_right_leaning_decreases(offsets, firsts) {
@@ -123,36 +93,51 @@ function make_right_leaning_decreases(offsets, firsts) {
 		if (offsets[index] > maxHill) {
 			//all regions larger than maxHill must be flattened
 			newOffsets[index] = maxHill;
+			//console.log("index " + index + " is flattened");
+			--index;
 		}
 		else if (offsets[index] < maxHill) {
 			//no need to flatten, reset and check for decrease region
 			maxHill = Infinity;
 			newOffsets[index] = offsets[index];
+			//console.log("setting stitch " + index);
 
-			if (offsets[index] > offsets[index+1]) {
+			if (offsets[index+1] >= offsets[index]) {
+				index--;
+				continue;
+			}
+
+			while (offsets[index] > offsets[index+1] && index >= 0) {
 				//in decreasing section
+				//console.log("found decrease at " + index);
 				maxHill = offsets[index];
 				if (firsts[index]) {
 					//fill to make center into right leaning
+					//console.log("decrease center is at " + index);
 					let minFloor = offsets[index];
 					let start = index+1;
 					while (start < offsets.length && newOffsets[start] < minFloor) {
+						console.log("filled " + start);
 						newOffsets[start] = minFloor;
 						++start;
 					}
 					//check to see if this was actually left leaning
 					if (offsets[index] >= offsets[index-1]) {
 						//if so, no need to flatten hills
+						//console.log("stop filling at " + index);
 						maxHill = Infinity;
 					}
 				}
+				--index;
+				newOffsets[index] = offsets[index];
 			}
 
 		}
 		else {
+			//console.log("index " + index + " is fine");
 			newOffsets[index] = offsets[index];
+			--index;
 		}
-		--index;
 	}
 
 	return newOffsets;
@@ -170,18 +155,23 @@ function make_left_leaning_decreases(offsets, firsts) {
 	while (index < offsets.length) {
 		if (offsets[index] < minFloor) {
 			newOffsets[index] = minFloor;
+			++index;
 		}
 		else if (offsets[index] > minFloor) {
 			minFloor = -Infinity;
 			newOffsets[index] = offsets[index]
-			if (offsets[index] < offsets[index-1]) {
+			if (offsets[index] >= offsets[index-1]) {
+				index++;
+				continue;
+			}
+			while (offsets[index] < offsets[index-1] && index < offsets.length) {
 				//in decreasing section
 				minFloor = offsets[index];
 				if (firsts[index]) {
 					//fill to make left leaning
 					let maxHill = offsets[index]
 					let start = index-1;
-					while (start => 0 && newOffsets[start] > maxHill) {
+					while (start >= 0 && newOffsets[start] > maxHill) {
 						newOffsets[start] = maxHill;
 						--start;
 					}
@@ -190,13 +180,15 @@ function make_left_leaning_decreases(offsets, firsts) {
 						minFloor = -Infinity;
 					}
 				}
+				index++;
+				newOffsets[index] = offsets[index];
 			}
 
 		}
 		else {
 			newOffsets[index] = offsets[index];
+			index++;
 		}
-		index++;
 	}
 
 	return newOffsets;
@@ -296,7 +288,7 @@ function print_goal(offsets, firsts) {
 	}
 	console.log(" index:" + infoI);
 	console.log("offset:" + infoO);
-	console.log(" first:" + infoF);
+	console.log(" first:" + infoF + "\n");
 }
 //-------------------------------------------------
 //testing code:
@@ -324,6 +316,7 @@ if (require.main === module) {
 			let minNeedle = 0;
 			let maxNeedle = offsets.length-1;
 			for (let n in needles) {
+				//if (needles[n].length == 0) continue;
 				let m = n.match(/^[fb](-?\d+)$/);
 				console.assert(m);
 				let val = parseInt(m[1]);
@@ -336,7 +329,7 @@ if (require.main === module) {
 				if (('b' + n) in needles) {
 					needles['b' + n].forEach(function(i, d){
 						while (d >= layers.length) layers.push("");
-						while (layers[d].length < n * 3) layers[d] += "   ";
+						while (layers[d].length < (n-minNeedle) * 3) layers[d] += "   ";
 						layers[d] += " ";
 						if (i < 10) layers[d] += " " + i;
 						else layers[d] += i;
@@ -351,7 +344,7 @@ if (require.main === module) {
 				if (('f' + n) in needles) {
 					needles['f' + n].forEach(function(i, d){
 						while (d >= layers.length) layers.push("");
-						while (layers[d].length < n * 3) layers[d] += "   ";
+						while (layers[d].length < (n-minNeedle) * 3) layers[d] += "   ";
 						layers[d] += " ";
 						if (i < 10) layers[d] += " " + i;
 						else layers[d] += i;
@@ -370,15 +363,17 @@ if (require.main === module) {
 				else if (n <  10) infoI += "  " + n.toString();
 				else              infoI += " " + n.toString();
 			}
-			console.log("index:" + infoI);
+			console.log("index:" + infoI + "\n");
+			
 		}
 		let log = [];
 
 		function xfer(fromBed, fromIndex, toBed, toIndex) {
 			let cmd = "xfer " + fromBed + fromIndex + " " + toBed + toIndex;
-			//console.log(cmd);
-			log.push(cmd);
 
+			console.log(cmd);
+			log.push(cmd);
+			
 			console.assert((fromBed === 'f' && toBed === 'b') || (fromBed === 'b' && toBed === 'f'), "must xfer f <=> b");
 
 			//check for valid racking:
@@ -409,6 +404,10 @@ if (require.main === module) {
 			}
 			console.assert(minRacking <= maxRacking, "there is a valid racking for this stitch configuration");
 			let racking = (fromBed === 'f' ? fromIndex - toIndex : toIndex - fromIndex);
+			if (racking < minRacking || racking > maxRacking) {
+				dumpNeedles();
+			}
+
 			console.assert(minRacking <= racking && racking <= maxRacking, "required racking " + racking + " is outside [" + minRacking + ", " + maxRacking + "] valid range. (" + cmd + ")");
 
 
@@ -417,11 +416,17 @@ if (require.main === module) {
 			if (!((toBed + toIndex) in needles)) needles[toBed + toIndex] = [];
 			var to = needles[toBed + toIndex];
 
-			console.assert(from.length !== 0, "no reason to xfer empty needle");
+			/*if (from.length == 0) {
+				//console.log("empty needle, not going to transfer");
+				return;
+			}
 			//console.assert(from.length === 1, "shouldn't ever xfer stack");
+			console.assert(from.length !== 0, "no reason to xfer empty needle");*/
+
 
 			while (from.length) to.push(from.pop());
 
+			
 			//dumpNeedles(); //DEBUG
 		}
 
@@ -432,6 +437,7 @@ if (require.main === module) {
 
 		multi_pass_transfers(offsets, firsts, minRacking, maxRacking, xfer);
 
+		print_goal(offsets, firsts);
 		dumpNeedles();
 
 		for (let i = 0; i < offsets.length; ++i) {
